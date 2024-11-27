d5f87a2215a8429255ce1c70f0494d3fe4f7252c
Mirjana Cale <mirjana083@gmail.com>
2024-11-27 15:17:56 +0000

    Add main.css style

--- blog/urls.py
+++ blog/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('',views.home, name='blog_home'),
-    path('about/', views.about, name='blog_about'),
+    path('',views.home, name='blog-home'),
+    path('about/', views.about, name='blog-about'),
     
     
 ]
