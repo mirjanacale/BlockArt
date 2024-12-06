422292911510c12b99fbbe848c0ce4fc5a8d9442
Mirjana Cale <mirjana083@gmail.com>
2024-12-06 22:42:32 +0000

    Fixing the difolt media image

--- blockstar/settings.py
+++ blockstar/settings.py
@@ -139,7 +139,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-MEDIA_ROOT = 'os.path.join(BASE_DIR, "media")'
+MEDIA_ROOT = '/workspace/BlockArt/media'
 MEDIA_URL = '/media/'
 
 # Default primary key field type
--- users/apps.py
+++ users/apps.py
@@ -4,3 +4,8 @@ from django.apps import AppConfig
 class UsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'users'
+
+def ready(self):
+    """This method is called when the application is ready. 
+    It imports the users.signals module to ensure that the signal handlers are connected."""
+    import users.signals   # noqa: F401
