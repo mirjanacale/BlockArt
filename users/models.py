1751019611d6892285e052cf4aec3dde5e9a0860
Mirjana Cale <mirjana083@gmail.com>
2024-12-06 20:12:40 +0000

    Change of modals

--- users/models.py
+++ users/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    bio = models.TextField(max_length=500, blank=True)
-    location = models.CharField(max_length=30, blank=True)
-    birth_date = models.DateField(null=True, blank=True)
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    
+    def __str__(self):
+        return f'{self.user.username} Profile'
