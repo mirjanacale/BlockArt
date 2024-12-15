d078a2302a300c16432909041f1341c9ab71801f
Mirjana Cale <mirjana083@gmail.com>
2024-12-15 22:52:45 +0000

    Add class-based views in order to create, update, and delete posts

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import (
     ListView,
     DetailView, 
     CreateView,
     UpdateView,
+    DeleteView
     )
 from .models import Post
 
@@ -31,7 +32,7 @@ class PostCreateView( LoginRequiredMixin,CreateView):
     model = Post 
     fields = ['title', 'content'] 
     
-class PostUpdateView( LoginRequiredMixin,UpdateView):
+class PostUpdateView( LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post 
     fields = ['title', 'content'] 
        
@@ -39,6 +40,23 @@ class PostUpdateView( LoginRequiredMixin,UpdateView):
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)  
+    
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+    
+class PostDeleteView( LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post 
+    success_url = '/'
+     
+    
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False     
     
     
         
