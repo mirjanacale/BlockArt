480f04e1fcc6cac863158bc3a97e8eb88d39adcd
MirjanaCale1 <mirjana083@gmail.com>
2025-05-07 22:45:43 +0100

    fix: remove duplicate Profile creation in register view, 500 errors ,Profile is now created automatically via post_save signal on User model

--- blog/admin.py
+++ blog/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 
 from .models import Post
 
-admin.site.register(Post)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'status', 'created_on')  # Display these fields in the admin list view
+    list_filter = ('status', 'author')  # Add filters for status and author
+    search_fields = ('title', 'content')  # Enable search by title and content
+    ordering = ('-created_on',)  # Order posts by creation date (newest first)
+
+
--- users/views.py
+++ users/views.py
@@ -10,7 +10,6 @@ def register(request):
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             user = form.save()
-            Profile.objects.create(user=user)
             username = form.cleaned_data.get('username')
             messages.success(request, f'Your account has been created! You are now able to log in, {username}!')
             return redirect('login')
