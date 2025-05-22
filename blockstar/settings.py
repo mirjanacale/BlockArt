23c70ef52609dab49d5d66dc82ada07480ecd638
MirjanaCale1 <mirjana083@gmail.com>
2025-05-22 22:01:42 +0100

    Make sure DATABASE_URL is set correctly in your env.py (for local) or Heroku Config Vars (for production)

--- blockstar/settings.py
+++ blockstar/settings.py
@@ -110,11 +110,12 @@ MIME_TYPES = {
 
 DATABASES = {
     'default': dj_database_url.config(
-        default='postgres://localhost',  # fallback for local dev
+        default=os.environ.get("DATABASE_URL"),
         conn_max_age=600,
-        ssl_require=True # change to True for production
+        ssl_require=True
     )
 }
+
 
 CSRF_TRUSTED_ORIGINS = ["https://*.codeinstitute-ide.net/", "https://*.herokuapp.com"]
 
