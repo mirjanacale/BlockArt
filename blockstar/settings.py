842e819f672dfe5f401057f4392813e33d8f7e4f
MirjanaCale1 <mirjana083@gmail.com>
2025-05-22 23:51:25 +0100

    add default

--- blockstar/settings.py
+++ blockstar/settings.py
@@ -101,12 +101,12 @@ MIME_TYPES = {
 
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+     'default': {
+         'ENGINE': 'django.db.backends.sqlite3',
+         'NAME': BASE_DIR / 'db.sqlite3',
+     }
+ }
 
 DATABASES = {
     'default': dj_database_url.config(
