8752378eca702e9cc63f03ca3d714d951013f5b7
MirjanaCale1 <mirjana083@gmail.com>
2025-06-11 22:59:26 +0100

    Enhance readability with content-wrapper layout in base.html and improve contrast styling

--- blog/views.py
+++ blog/views.py
@@ -171,26 +171,6 @@ def newsletter_signup(request):
     return render(request, 'subscribe.html', {'form': form})
 
 
-def post_detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    comments = post.blog_comments.all()  
-    if request.method == 'POST':
-        comment_form = CommentForm(request.POST)
-        if comment_form.is_valid():
-            new_comment = comment_form.save(commit=False)
-            new_comment.post = post
-            new_comment.user = request.user
-            new_comment.save()
-            return redirect('post-detail', pk=post.pk)
-    else:
-        comment_form = CommentForm()
-    return render(request, 'blog/post_detail.html', {
-        'object': post,
-        'comments': comments,
-        'comment_form': comment_form,
-    })
-
-
 def post_create(request):
     if request.method == 'POST':
         form = PostForm(request.POST, request.FILES)
